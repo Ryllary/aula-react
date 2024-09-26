4f4fa3131776e73b74686c7a35f077d69964e24b
ryllary <victoriaryllary103@gmail.com>
2024-09-26 09:57:05 -0300

    FINALIZAR

--- src/components/SearchVideoList/index.js
+++ src/components/SearchVideoList/index.js
@@ -1,21 +1,21 @@
 import styles from "./SearchVideoList.module.css";
 import VideoList from "../../components/VideoList";
-import { useEffect, useState } from "react";
-import Loader from "../Loader";
+import Loader from "../../components/Loader";
+import {useEffect, useState} from "react";
 
-// filtrando videos por categoria ou titulo
-function filterVideos(videos, searchText) {
+// aqui vc filtra os videos por category ou title
+function filterVideos(videos, searchText){
     return videos.filter((video) => video.category.includes(searchText) || video.title.includes(searchText))
-}
 
+}
 function SearchVideoList({ videos }) {
 
     const [ searchText, setSearchText ] = useState('')
     const foundVideos = filterVideos(videos, searchText)
     
-    const [ loading, setLoading ] = useState(true)
-    useEffect(() => {
-        setTimeout(() => setLoading(false), 500)
+    const [loading, setLoading] = useState(true)
+    useEffect(()=>{
+        setTimeout(() => setLoading(false), 600)
     }, [])
 
     return (
@@ -26,13 +26,13 @@ function SearchVideoList({ videos }) {
                 value={searchText}
                 onChange={event => setSearchText(event.target.value)}
             />
-            {
-                loading ? <Loader /> : 
+            { 
+                loading ? <Loader/> :
                 <VideoList
                     videos={foundVideos}
                     emptyHeading={`Sem vídeos sobre "${searchText}"`}
                 />
-            }
+            } 
         </section>
     );
 }
